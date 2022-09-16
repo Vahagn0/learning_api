b391898d295cfe102c27291faa8d78909d7dc222
Vahagn0 <petrosyanvav@gmail.com>
2022-09-16 15:24:44 +0400

    added config.js file

---
+++ config.js
@@ -0,0 +1,2 @@
+export let baseUrl = "http://universities.hipolabs.com"
+ //const url = "/search?country=United+States"
--- data.js
+++ data.js
@@ -1,43 +1,48 @@
-let main = document.getElementById("main")
+//let main = document.getElementById("main")
+import baseUrl from './config.js'
+console.log(baseUrl)
 
-async function fetchData(){
-    const datas = await fetch("http://universities.hipolabs.com/search?country=United+States").then(response => {
-        return response.json();
-    })
 
-    const finalData = datas.map(data => {
-        return {
-            name: data.name,
-            country:data.country
-        }
-    })
+// async function fetchData(){
+//     const datas = await fetch("http://universities.hipolabs.com/search?country=United+States").then(response => {
+//         return response.json();
+//     })
+
+//      const finalData = datas.map(data => {
+//         return {
+//             name: data.name,
+//             country:data.country
+//         }
+//     })
     
-    console.log(finalData)
-    const divv = document.createElement("div");
+//     console.log(finalData)
+//     const divv = document.createElement("div");
    
-        finalData.forEach( university => { 
-            let ul = document.createElement("ul");
-            let country = document.createElement("li")
-            main.appendChild(ul).innerHTML = university.name
-            ul.style.color = "red"
-            country.style.color = "blue"
-             ul.onclick = () => { 
-                ul.appendChild(country).innerHTML = university.country
-             };
+//         finalData.forEach( university => { 
+//             let ul = document.createElement("ul");
+//             let country = document.createElement("li")
+//             main.appendChild(ul).innerHTML = university.name
+//             ul.style.color = "red"
+//             country.style.color = "blue"
+//              ul.onclick = () => { 
+//                 ul.appendChild(country).innerHTML = university.country
+//              };
 
-        });
+//         });
         
 
 
-}
+// }   
 
-fetchData()
-
-
+// fetchData()
 
 
 
-// example
+
+
+// Example
 // divv = document.createElement("div")
 // main.innerHTML = "poxoooos"
 // main.appendChild(divv).innerHTML = "pedros"
+// getway
+
